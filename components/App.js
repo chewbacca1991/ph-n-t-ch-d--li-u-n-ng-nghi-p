4b540dd97b1c4b7618729bfbadae1c051dd5f352
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-25 18:13:30 +0330

    feat: initial commit for components/App.js

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Phân Tích Dữ Liệu Nông Nghiệp</h1>
+      <p>Chào mừng bạn đến với ứng dụng phân tích dữ liệu nông nghiệp!</p>
+    </div>
+  );
+};
+
+export default App;
