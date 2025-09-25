62dc05e63aa70ea1ba4bffecf8afcae0a4355187
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-25 18:13:32 +0330

    feat: initial commit for components/DataCollection.js

---
+++ components/DataCollection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataCollection = () => {
+  return (
+    <div>
+      <h2>Thu Thập Dữ Liệu</h2>
+      <p>Đây là nơi bạn sẽ thu thập dữ liệu từ nhiều nguồn khác nhau.</p>
+    </div>
+  );
+};
+
+export default DataCollection;
