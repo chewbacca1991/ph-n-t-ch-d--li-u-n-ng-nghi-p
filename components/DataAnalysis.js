39ccc8a472851ec18f3d1d666cef0b9f87d2e79c
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-25 18:13:33 +0330

    feat: initial commit for components/DataAnalysis.js

---
+++ components/DataAnalysis.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DataAnalysis = () => {
+  return (
+    <div>
+      <h2>Phân Tích Dữ Liệu</h2>
+      <p>Đây là nơi bạn sẽ thực hiện phân tích dữ liệu nông nghiệp.</p>
+    </div>
+  );
+};
+
+export default DataAnalysis;
